c965ee746d8eadf5a8eda0d416ecc3e4ed18f495
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-07-03 12:25:01 +0200

    Update game.sh

--- game.sh
+++ game.sh
@@ -4,3 +4,12 @@
 echo Welcome to this simple game! - Whats your name?
 read varname1
 echo Nice to meet you $varname1! - Whats 2+2?
+read varmane2
+if [[$varname2 == 4]] than
+echo Thats correct.
+  else echo NOPE. Try again.
+  read varname3
+  if [[$varname3 == 4]]
+  echo thats right
+ fi 
+fi
