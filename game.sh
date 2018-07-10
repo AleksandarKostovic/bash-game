dfa543b4a546dfd842778480aebe8b77ce48b4e1
AleksandarKostovic <32797225+AleksandarKostovic@users.noreply.github.com>
2018-07-10 14:54:22 +0200

    Update game.sh

--- game.sh
+++ game.sh
@@ -9,12 +9,12 @@ read varname2
 
 if [[ $varname2 == 4 ]]; 
 
-then	
-	echo Thats correct
-else	
-	echo NOPE, try again.	
+ then	echo Thats correct
+    
+    else	echo NOPE, try again.	
 
 read varname3	
+
 if [[ $varname3 == 4 ]]; then		
 	echo Whooho, you rock!	
 fi
